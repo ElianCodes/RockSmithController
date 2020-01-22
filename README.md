560fa5e6ce5d05194ca090b8f8462440874b08ab
Elian Van Cutsem <elian.vancutsem@student.odisee.be>
2020-01-22 18:18:48 +0100

    merge from gitlab

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # RockSmithController
+
+This is the source code for a MIDI controller to control Rocksmith2014
+
+## table of contents
+
+1.[Basic idea](#idea)
+2.[functions](#functions)
+3.[requirements](#requirements)
+4.[code](#code)
+5.[howto](#howto)
+
+## idea
+
+This project is based of the game rocksmith 2014. The perpose of the game is to learn you guiter while having a GUI play-along on your tv or monitor.
+The game requires a real electric guitar to work and a keyboard to navigate the menus ect. I played this game for almost 20 hours and noticed what was missing for me.
+While playing the guitar standing, I dont have my keyboard at hand at every moment, while playing guitar seated, I don't really have the space to move around that much. So I tought of a solution to fix my problem. An Arduino based stompbox which contains all the functions used in the game.
+
+## functions
+
+The game funtions are fairly limited, I've listed them all below.
+
+2 buttons to represent the up and down arrow keys - used to navigate on every screen
+2 buttons to represent the right and left keys - these get used to choose a style to play / can be used to riffrepeat
+esc button - back button on most screens
+spacebar button - tools and more
+ctrl button - change input or guitar
+enter button - obvious
+delete - gets used for certain options like skip tuner and account menu
+\+ - gets used to favourite items in the game. Not sure I'm going to implement this.
+
+----------------------
+
+4 arrows + esc + space + ctrl + enter + del = 4 + 5 = 9
+
+----------------------
+
+maybe a volume control? = 10 buttons
+maybe a favourite button = 11 buttons max
+
+## requirements
+
+This projects needs a certain list of items. I've listed them below:
+
+- An Arduino Uno
+- Atmel Flip (software)
+- arduino Uno R3 firmware (Located in the firmware folder)
+- a pdf of HID usage tables (located in master of this project)
+- Basics of arduino skills
+- type of buttons you want to use
+- electrical wires
+- a led
+
+## code
+
+All code is inside of controller.ino
+Here's how it works:
+
+## howto
+
+Here's how to make this project for yourself:
+
+## ideas & todo
+
+- Rotating button as volume
+- Audio Jack troughput
+- USB troughput or guitar troughput
+- ON/OFF pickup selector
+
+StompSwitches for normal keys
+<https://www.banggood.com/5Pcs-Electric-Guitar-Effect-Momentary-Push-Button-Stomp-Foot-Pedal-Switch-p-1134006.html?rmmds=search&cur_warehouse=CN>
+
+Search other option for arrows
+
+    _________________________
+    |                       |
+    |       x   x   x       |
+    |                       |
+    |   x   x   x   x   x   |
+    |_______________________|
---
+++ controller/controller.ino
@@ -0,0 +1,203 @@
+#include <Arduino.h>
+
+//Buttons
+int PIN_UP = 12;
+int PIN_DOWN = 11;
+int PIN_RIGHT = 9;
+int PIN_LEFT = 10;
+int PIN_ENTER = 1;
+int PIN_ESC = 3;
+int PIN_CTRL = 5;
+int PIN_SPACE = 7;
+int PIN_DEL = 8;
+#define statusLed 13
+
+//keys to press
+#define KEY_UP 82
+#define KEY_DOWN 81
+#define KEY_RIGHT 79
+#define KEY_LEFT 80
+#define KEY_ENTER 40
+#define KEY_ESC 41
+#define KEY_CTRL 224
+#define KEY_SPACE 44
+#define KEY_DEL 42
+
+/*  Keyboard report buffer
+    DO NOT CHANGE     */
+uint8_t buf[8] = { 0 };
+
+/*  state of button
+    DO NOT CHANGE   */
+int state = 1;
+bool btnPressed = false;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(PIN_UP, INPUT);
+  pinMode(PIN_DOWN, INPUT);
+  pinMode(PIN_LEFT, INPUT);
+  pinMode(PIN_RIGHT, INPUT);
+  pinMode(PIN_ENTER, INPUT);
+  pinMode(PIN_ESC, INPUT);
+  pinMode(PIN_CTRL, INPUT);
+  pinMode(PIN_SPACE, INPUT);
+  pinMode(PIN_DEL, INPUT);
+
+
+  pinMode(statusLed, OUTPUT);
+
+  // enable internal pull-ups
+  digitalWrite(PIN_UP, 1);
+  digitalWrite(PIN_DOWN, 1);
+  digitalWrite(PIN_RIGHT, 1);
+  digitalWrite(PIN_LEFT, 1);
+  digitalWrite(PIN_ENTER, 1);
+  digitalWrite(PIN_ESC, 1);
+  digitalWrite(PIN_CTRL, 1);
+  digitalWrite(PIN_SPACE, 1);
+  digitalWrite(PIN_DEL, 1);
+
+  delay(200);
+}
+
+void loop()
+{
+  arrowUp();
+  checkPressed(PIN_UP);
+  arrowDown();
+  checkPressed(PIN_DOWN);
+  arrowRight();
+  checkPressed(PIN_RIGHT);
+  arrowLeft();
+  checkPressed(PIN_LEFT);
+  enterKey();
+  checkPressed(PIN_ENTER);
+  escKey();
+  checkPressed(PIN_ESC);
+  ctrlKey();
+  checkPressed(PIN_CTRL);
+  spaceKey();
+  checkPressed(PIN_SPACE);
+  deleteKey();
+  checkPressed(PIN_DEL);
+}
+
+void deleteKey(){
+  state = digitalRead(PIN_DEL);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_DEL;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void spaceKey(){
+  state = digitalRead(PIN_SPACE);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_SPACE;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void ctrlKey(){
+  state = digitalRead(PIN_CTRL);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_CTRL;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void escKey(){
+  state = digitalRead(PIN_ESC);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_ESC;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void enterKey(){
+  state = digitalRead(PIN_ENTER);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_ENTER;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void arrowUp(){
+  state = digitalRead(PIN_UP);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_UP;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void arrowDown(){
+  state = digitalRead(PIN_DOWN);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_DOWN;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void arrowLeft(){
+  state = digitalRead(PIN_LEFT);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_LEFT;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void arrowRight(){
+  state = digitalRead(PIN_RIGHT);
+  if (state != 1 && !btnPressed) {
+    btnPressed = true;
+    buf[2] = KEY_RIGHT;   // set key to press
+    Serial.write(buf, 8); // Send keypress
+    releaseKey(); //loose press
+  }
+}
+
+void checkPressed(int pinNumber){
+  while (state != 1){
+    state = digitalRead(pinNumber);
+    delay(20);
+  }
+   if(state == 1 && btnPressed){
+    btnPressed = false;
+  }
+}
+
+void blinkLed() {
+  for (int i = 0; i < 3 ; i++) {
+    digitalWrite(statusLed, HIGH);
+    delay(100);
+    digitalWrite(statusLed, LOW);
+    delay(100);
+  }
+}
+
+/* DO NOT CHANGE */
+void releaseKey()
+{
+  buf[0] = 0;
+  buf[2] = 0;
+  Serial.write(buf, 8); // Release key
+  blinkLed();
+}
